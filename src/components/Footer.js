28369099ea2ffa1e419d99cdb6495b1236f6bf7f
Arnaud <arnaud.malfait89@gmail.com>
2021-04-02 19:39:56 +0200

    final commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <a target="blank" href="https://boring-jackson-1a88d8.netlify.app">
+          To-Do-List de Arnaud Malfait
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
